d1bb277a7aaaa77a4a0c4c40318aa7eff8a41972
sandinosaso <sandinosaso@gmail.com>
2014-07-21 11:51:42 -0300

    My first commit to repo from local version

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+  db:{
+    host:"localhost",
+    database:"duydwh",
+    username:"postgres",
+    password:"postgres"
+  },
+}
+module.exports = config;
---
+++ public/chat.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+ 
+    var messages = [];
+    var host   = 'locahost';
+    var port   = '3700';
+
+    var socket = io.connect('http://'+host+':'+port);
+    var field = document.getElementById("field");
+    var sendButton = document.getElementById("send");
+    var content = document.getElementById("content");
+ 
+    socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data.message);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += messages[i] + '<br />';
+            }
+            content.innerHTML = html;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+ 
+    sendButton.onclick = function() {
+        var text = field.value;
+        socket.emit('send', { message: text });
+    };
+ 
+}
